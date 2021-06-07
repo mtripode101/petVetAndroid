7d0fe5015aee6954bf006f72f7638e5a2a42d777
Martin Tripode <martin.tripode@entrepids.com>
2021-06-07 12:21:43 -0300

    addind doctor model and rest

---
+++ app/src/main/java/com/mtripode/pettest1/entity/Doctor.java
@@ -0,0 +1,13 @@
+package com.mtripode.pettest1.entity;
+
+public class Doctor extends Customer {
+    private String cedula;
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+}
