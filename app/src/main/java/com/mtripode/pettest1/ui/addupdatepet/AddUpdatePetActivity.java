4bdcd63c26ff668476f4dad0a1ff674122bbc2cf
Martin Tripode <martin.tripode@entrepids.com>
2021-07-07 12:40:27 -0300

    adding updating datepicker birthday

--- app/src/main/java/com/mtripode/pettest1/ui/addupdatepet/AddUpdatePetActivity.java
+++ app/src/main/java/com/mtripode/pettest1/ui/addupdatepet/AddUpdatePetActivity.java
@@ -2,10 +2,12 @@ package com.mtripode.pettest1.ui.addupdatepet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.mtripode.pettest1.R;
@@ -14,9 +16,13 @@ import com.mtripode.pettest1.entity.Customer;
 import com.mtripode.pettest1.errors.ConnectionError;
 import com.mtripode.pettest1.service.AnimalServiceImpl;
 import com.mtripode.pettest1.ui.petmenu.PetMenuActivity;
+import com.mtripode.pettest1.ui.register.RegisterActivity;
 import com.mtripode.pettest1.utils.SessionUtils;
 import com.mtripode.pettest1.validators.AnimalValidator;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 
 public class AddUpdatePetActivity extends AppCompatActivity {
@@ -27,6 +33,10 @@ public class AddUpdatePetActivity extends AppCompatActivity {
     private TextView editTextAddUpdatePetName;
     private TextView editTextAddUpdatePetSex;
     private TextView editTextAddUpdatePetSpecie;
+    private TextView petRegisterTextDateBirthday;
+    private DatePickerDialog picker;
+    private Date dateBirthday;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +57,33 @@ public class AddUpdatePetActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 buttonAddUpdatePetBack(v);
+            }
+        });
+
+        this.petRegisterTextDateBirthday = findViewById(R.id.petRegisterTextDateBirthday);
+        Button petRegisterGetButton = findViewById(R.id.petRegisterGetButton);
+        petRegisterGetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(AddUpdatePetActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                petRegisterTextDateBirthday.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                                Calendar calendar = Calendar.getInstance();
+                                calendar.set(year, monthOfYear, day);
+
+                                SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+                                dateBirthday = calendar.getTime();
+
+                            }
+                        }, year, month, day);
+                picker.show();
             }
         });
 
@@ -84,12 +121,15 @@ public class AddUpdatePetActivity extends AppCompatActivity {
             animalToupdate.setSex(this.editTextAddUpdatePetSex.getText().toString());
             animalToupdate.setName(this.editTextAddUpdatePetName.getText().toString());
             animalToupdate.setSpecie(this.editTextAddUpdatePetSpecie.getText().toString());
+            animalToupdate.setBirthday(this.dateBirthday);
 
             AnimalValidator validator = new AnimalValidator();
             HashMap<String, Object> elements = new HashMap<>();
             elements.put("editTextAddUpdatePetSex", editTextAddUpdatePetSex);
             elements.put("editTextAddUpdatePetName", editTextAddUpdatePetName);
             elements.put("editTextAddUpdatePetSpecie", editTextAddUpdatePetSpecie);
+            elements.put("petRegisterTextDateBirthday",petRegisterTextDateBirthday);
+            elements.put("dateBirthday", dateBirthday);
             Boolean hasError = validator.validate(animalToupdate, elements);
             if (hasError.equals(Boolean.FALSE)){
                 if (Boolean.FALSE.equals(isNew)){
