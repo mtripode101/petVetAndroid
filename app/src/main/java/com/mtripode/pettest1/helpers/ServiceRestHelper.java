f529c328c751a3e5dd1b8c1811521ab96dfe00e9
Martin Tripode <martin.tripode@entrepids.com>
2021-07-13 10:58:53 -0300

    json converter

--- app/src/main/java/com/mtripode/pettest1/helpers/ServiceRestHelper.java
+++ app/src/main/java/com/mtripode/pettest1/helpers/ServiceRestHelper.java
@@ -4,10 +4,12 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.mtripode.pettest1.service.RestInterface;
 
+import java.lang.reflect.Type;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Converter;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -42,11 +44,35 @@ public class ServiceRestHelper {
                 .build();
     }
 
+    public Retrofit createWithAdapter (Type type, Object typeAdapter){
+        httpClient = new OkHttpClient().newBuilder().build();
+        httpClient.newBuilder().connectTimeout(60, TimeUnit.SECONDS);
+        httpClient.newBuilder().readTimeout(60, TimeUnit.SECONDS);
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        return new Retrofit.Builder()
+                .baseUrl(BASEPOINT)
+                .client(httpClient)
+                .addConverterFactory(createGsonConverter(type,typeAdapter))
+                .build();
+    }
+
     public RestInterface create (){
         return ServiceRestHelper.getInstance().createAdapter().create(RestInterface.class);
     }
 
+    public RestInterface createWithTypeAdapter (Type type, Object typeAdapter){
+        return ServiceRestHelper.getInstance().createWithAdapter(type,typeAdapter).create(RestInterface.class);
+    }
 
+    private static Converter.Factory createGsonConverter(Type type, Object typeAdapter) {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(type, typeAdapter);
+        Gson gson = gsonBuilder.setDateFormat("yyyy-MM-dd").create();
+
+        return GsonConverterFactory.create(gson);
+    }
 
 
 }
---
+++ app/src/main/java/com/mtripode/pettest1/jsonutils/CustomerDesrializer.java
@@ -0,0 +1,115 @@
+package com.mtripode.pettest1.jsonutils;
+
+import android.annotation.SuppressLint;
+import android.util.ArraySet;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.mtripode.pettest1.entity.Customer;
+import com.mtripode.pettest1.entity.Doctor;
+
+import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CustomerDesrializer implements JsonDeserializer<Customer> {
+
+
+    @Override
+    public Customer deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        final JsonObject jsonObject = json.getAsJsonObject();
+
+        if (jsonObject.get("cedula") != null){
+            String cedula = jsonObject.get("cedula").getAsString();
+            Doctor doctor = this.getDoctor();
+            this.setCommonValues(jsonObject, doctor);
+            doctor.setCedula(cedula);
+            return doctor;
+        }
+        else{
+            Customer customer = this.getCustomer();
+            this.setCommonValues(jsonObject, customer);
+            return customer;
+        }
+
+    }
+
+    private Customer getCustomer (){
+        return new Customer();
+    }
+
+    private Doctor getDoctor (){
+        return new Doctor();
+    }
+
+    @SuppressLint("NewApi")
+    private void setCommonValues (JsonObject jsonObject, Customer customer){
+        final Long id = jsonObject.get("id").getAsLong();
+        customer.setId(id);
+
+        Date birthDate = null;
+        try {
+            String dateStr = jsonObject.get("birthday").getAsString();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            birthDate = sdf.parse(dateStr);
+        } catch (Exception e) {
+            birthDate = new Date();
+        }
+
+        customer.setBirthday(birthDate);
+        if (!this.isNullValue(jsonObject.get("lastName"))){
+            customer.setLastName(jsonObject.get("lastName").getAsString());
+        }
+
+        if (!this.isNullValue((jsonObject.get("email")))){
+            customer.setEmail(jsonObject.get("email").getAsString());
+        }
+
+        if (!this.isNullValue(jsonObject.get("username"))){
+            customer.setUsername(jsonObject.get("username").getAsString());
+        }
+
+        if (!this.isNullValue(jsonObject.get("name"))){
+            customer.setName(jsonObject.get("name").getAsString());
+        }
+
+        if (!this.isNullValue(jsonObject.get("password"))){
+            customer.setPassword(jsonObject.get("password").getAsString());
+        }
+
+        if (!this.isNullValue(jsonObject.get("celphone1"))){
+            customer.setCelphone1(jsonObject.get("celphone1").getAsString());
+        }
+
+        if (!this.isNullValue(jsonObject.get("celphone2"))){
+            customer.setCelphone2(jsonObject.get("celphone2").getAsString());
+        }
+
+        if (!this.isNullValue(jsonObject.get("celphone3"))){
+            customer.setCelphone3(jsonObject.get("celphone3").getAsString());
+        }
+
+        if (!this.isNullValue(jsonObject.get("sex"))){
+            customer.setSex(jsonObject.get("sex").getAsString());
+        }
+
+        if (!this.isNullValue(jsonObject.get("enable"))){
+            customer.setEnable(jsonObject.get("enable").getAsBoolean());
+        }
+
+        customer.setUserClose(new Date());
+        customer.setUserCreation(new Date());
+        customer.setAnimals(new ArraySet<>());
+        customer.setDoctors(new ArraySet<>());
+        customer.setAddresses(new ArraySet<>());
+        customer.setHospital(new ArraySet<>());
+    }
+
+    private boolean isNullValue (Object obj){
+        return obj.toString().equals("null");
+    }
+}
